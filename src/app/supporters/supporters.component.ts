cf4aba6f04f410b938e66ef2b9b996480d0c3c51
indrajeet1992 <indrajeetchoudhury1992@gmail.com>
2020-11-17 00:50:12 +0530

    Updated leadership & supporters page.
    Contact us
    header allignment
    leadership
    supporters

--- src/app/supporters/supporters.component.ts
+++ src/app/supporters/supporters.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery'
 @Component({
   selector: 'app-supporters',
   templateUrl: './supporters.component.html',
@@ -10,6 +10,18 @@ export class SupportersComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $('.counter-count').each(function () {
+      $(this).prop('Counter',0).animate({
+          Counter: $(this).text()
+      }, {
+          duration: 5000,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(Math.ceil(now));
+          }
+      });
+  });
   }
+
 
 }
