9c2765d464ba982b85c90ecb65f93fa16c3b9f37
Sukhchand Kisku <skisku@manh.com>
2020-12-29 21:10:26 +0530

    Manage user service

---
+++ src/app/edit-profile/components/manage-users/services/manage-users.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { BASE_URL } from 'src/app/constant';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ManageUsersService {
+
+  constructor(private http: HttpClient) { }
+
+  public getUsers() {
+    const path = `${BASE_URL}/user`;
+    return this.http
+      .get(path)
+      .pipe(map((response: any) => response));
+  }
+
+  public activeUser(userData:any, isActive:boolean) {
+    const path = `${BASE_URL}/active/${userData.id}/${isActive}`;
+    return this.http
+      .put(path,{})
+      .pipe(map((response: any) => response),
+      catchError((errorRes) => {
+        console.log(errorRes);
+        return throwError(errorRes.error.message);
+      }));
+  }
+}
