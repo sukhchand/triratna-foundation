5edaa00beaefb8afe7452d29e70594cda031a3e3
Vishal-Chhetri <42584441+Vishal-Chhetri@users.noreply.github.com>
2021-03-21 15:56:07 +0530

    adding more location for contact us and image for food and shelter

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -41,6 +41,46 @@ export class ContactusComponent implements OnInit {
         lat: 26.7311718,
         lng: 87.9804833,
         address:"Arjundhara-3, Jhapa, Nepal"
+      },      
+      {
+        lat: 40.2820984,
+        lng: -80.0922118,
+        address:"375 Valley Brook Road, MCMURRAY, PA 15317"
+      },      
+      {
+        lat: 26.7311718,
+        lng: 87.9804833,
+        address:"2714 Parkview Place, Lewisville, Texas, 75067"
+      },      
+      {
+        lat: 33.025054931640625,
+        lng: -97.08573150634766,
+        address:"Chauntra, District Mandi, India Himachal Pradesh"
+      },      
+      {
+        lat: -40.3658,
+        lng: 175.5885,
+        address:"142A Rugby st, Awapuni, post code 4412, Palmerston North, New Zealand"
+      },      
+      {
+        lat: 33.61803,
+        lng: 130.42655,
+        address:"Fukuoka city Higashi-ku Hakozaki Memorial Hakozaki 503, Japan"
+      },      
+      {
+        lat: 27.75,
+        lng: 86.75,
+        address:"Sagardanda, 05 Likhupike, Solukhumbu, Nepal"
+      },      
+      {
+        lat: 23.10787,
+        lng: 91.97007,
+        address:"Wakchari, Manikchhari, Khagrachhari, Bangladesh"
+      },      
+      {
+        lat: 37.9949705,
+        lng: -105.7009757,
+        address:"Crestone Colorado 81131, USA"
       }
     ]
   };
